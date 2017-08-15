36c962388fd1d48ad5854ac9baef311be2cbf09c
lihe <lwk14201@163.com>
2017-08-15 19:50:21 +0800

    version1.01

---
+++ img/echo.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+</head>
+
+<body>
+<?php 
+	echo "<div style='width:50px;height:50px;background:#ccc;position:relative;left:5px;top:5px;'>我是div</div>";
+	
+?>
+</body>
+</html>
